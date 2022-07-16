3cbeab84ff0b8d871275936d4d5a6d4bc7ca07a0
Alexandr Machilov <machilov00@mail.ru>
2022-07-16 16:17:22 +0300

    fix: password length

--- backend/middlewares/validation.js
+++ backend/middlewares/validation.js
@@ -11,12 +11,12 @@ const urlValidation = (value, helper) => {
 const registerValidation = celebrate({
   [Segments.BODY]: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
     avatar: Joi.string()
       .default(
-        'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+        'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'
       )
       .custom(urlValidation)
       .messages({ 'string.notURL': 'Некорректный URL' }),
@@ -26,7 +26,7 @@ const registerValidation = celebrate({
 const loginValidation = celebrate({
   [Segments.BODY]: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(),
+    password: Joi.string().required(),
   }),
 });
 
